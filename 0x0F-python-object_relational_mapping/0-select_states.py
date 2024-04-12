3574e91481d87f70558d026dd3af72ff02bbb488
EmmanuelNyabuto <emmanuele2ke@gmail.com>
2024-04-12 05:12:06 -0700

    Edited Pycode

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """Module that lists all states from mySQL database"""
+
 import sys
 import MySQLdb
 
-def list_states (username, password, database):
+
+def list_states(username, password, database):
     """lists all states from the database hbtn_0e_0_usa.
-    Ags:
+    Args:
         username: mysql username
         password: mysql password
         database: mysql database
     """
     # Connect to the MySQL server
-    db = MySQLdb.connect(host='localhost',\
-            port=3306,\
-            user=username,\
-            passwd=password,\
-            db=database)
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
     cursor = db.cursor()
 
     # Execute the SQL query to fetch all states
@@ -31,9 +35,9 @@ def list_states (username, password, database):
     # Close the database connection
     db.close()
 
+
 # Example usage
 if __name__ == '__main__':
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
