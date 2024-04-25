a3946ac48b11ab5341c133e7dd14fc19fbcb2a5a
EmmanuelNyabuto <emmanuele2ke@gmail.com>
2024-04-25 15:37:18 -0700

    try3

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes in a URL as an argument, sends a GET request to the URL,and displays the body of the response
-curl -sH "X-School-User-Id: 98" "$1"[2;2R[>0;10;1c
+# Sends a GET request to the URL, and displays the body of the response
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
